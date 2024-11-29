c1be19a9547a3590bee65e162a6ded1fab24b00a
Seil Oh <hardboiledbaby@gmail.com>
2024-11-29 16:39:25 +0900

    add selfterminate

--- Sources/Asynchrone/Sequences/SharedAsyncSequence.swift
+++ Sources/Asynchrone/Sequences/SharedAsyncSequence.swift
@@ -31,13 +31,14 @@ import Foundation
 /// let values = try await self.stream.collect()
 /// // ...
 /// ```
-public struct SharedAsyncSequence<Base: AsyncSequence>: AsyncSequence, Sendable where Base: Sendable {
+public struct SharedAsyncSequence<Base: AsyncSequence>: AsyncSequence, Sendable
+where Base: Sendable {
     /// The type of async iterator.
     public typealias AsyncIterator = AsyncThrowingStream<Base.Element, Error>.Iterator
-    
+
     /// The type of elements streamed.
     public typealias Element = Base.Element
-    
+
     // Private
     private var base: Base
     private let manager: SubSequenceManager<Base>
@@ -47,13 +48,13 @@ public struct SharedAsyncSequence<Base: AsyncSequence>: AsyncSequence, Sendable 
     /// Creates a shareable async sequence that can be used across multiple tasks.
     /// - Parameters:
     ///   - base: The async sequence in which this sequence receives it's elements.
-    public init(_ base: Base) {
+    public init(_ base: Base, selfTerminate: Bool = false) {
         self.base = base
-        self.manager = SubSequenceManager<Base>(base)
+        self.manager = SubSequenceManager<Base>(base, selfTerminate: selfTerminate)
     }
-    
+
     // MARK: AsyncSequence
-    
+
     /// Creates an async iterator that emits elements of this async sequence.
     /// - Returns: An instance that conforms to `AsyncIteratorProtocol`.
     public func makeAsyncIterator() -> AsyncThrowingStream<Base.Element, Error>.Iterator {
@@ -70,17 +71,17 @@ extension SharedAsyncSequence {
     /// along with emitting it through the sequence.
     /// - Parameter element: The element to yield.
     public func yield<Element>(_ element: Element) async
-    where Base == CurrentElementAsyncSequence<Element>, Element: Sendable  {
+    where Base == CurrentElementAsyncSequence<Element>, Element: Sendable {
         await self.base.yield(element)
     }
-    
+
     /// Mark the sequence as finished by having it's iterator emit nil.
     ///
     /// Once finished, any calls to yield will result in no change.
     public func finish<Element>() async where Base == CurrentElementAsyncSequence<Element> {
         await self.base.finish()
     }
-    
+
     /// Emit one last element beford marking the sequence as finished by having it's iterator emit nil.
     ///
     /// Once finished, any calls to yield will result in no change.
@@ -89,7 +90,7 @@ extension SharedAsyncSequence {
     where Base == CurrentElementAsyncSequence<Element>, Element: Sendable {
         await self.base.finish(with: element)
     }
-    
+
     /// The element wrapped by this async sequence, emitted as a new element whenever it changes.
     public func element<Element>() async -> Element
     where Base == CurrentElementAsyncSequence<Element>, Element: Sendable {
@@ -104,22 +105,23 @@ extension SharedAsyncSequence {
     ///
     /// Yielding a new element will emit it through the sequence.
     /// - Parameter element: The element to yield.
-    public func yield<Element>(_ element: Element) where Base == PassthroughAsyncSequence<Element>  {
+    public func yield<Element>(_ element: Element) where Base == PassthroughAsyncSequence<Element> {
         self.base.yield(element)
     }
-    
+
     /// Mark the sequence as finished by having it's iterator emit nil.
     ///
     /// Once finished, any calls to yield will result in no change.
     public func finish<Element>() where Base == PassthroughAsyncSequence<Element> {
         self.base.finish()
     }
-    
+
     /// Emit one last element beford marking the sequence as finished by having it's iterator emit nil.
     ///
     /// Once finished, any calls to yield will result in no change.
     /// - Parameter element: The element to emit.
-    public func finish<Element>(with element: Element) where Base == PassthroughAsyncSequence<Element> {
+    public func finish<Element>(with element: Element)
+    where Base == PassthroughAsyncSequence<Element> {
         self.base.finish(with: element)
     }
 }
@@ -131,55 +133,65 @@ fileprivate actor SubSequenceManager<Base: AsyncSequence> where Base: Sendable {
 
     // Private
     private var base: Base
-    private var continuations: [String : AsyncThrowingStream<Base.Element, Error>.Continuation] = [:]
+    private var selfTerminate: Bool
+    private var continuations: [String: AsyncThrowingStream<Base.Element, Error>.Continuation] = [:]
     private var subscriptionTask: Task<Void, Never>?
 
     // MARK: Initialization
 
-    fileprivate init(_ base: Base) {
+    fileprivate init(_ base: Base, selfTerminate: Bool) {
         self.base = base
+        self.selfTerminate = selfTerminate
     }
-    
+
     deinit {
         self.subscriptionTask?.cancel()
     }
-    
+
     // MARK: API
-    
+
     /// Creates an new stream and returns its async iterator that emits elements of base async sequence.
     /// - Returns: An instance that conforms to `AsyncIteratorProtocol`.
-    nonisolated fileprivate func makeAsyncIterator() -> ThrowingPassthroughAsyncSequence<Base.Element>.AsyncIterator {
+    nonisolated fileprivate func makeAsyncIterator()
+        -> ThrowingPassthroughAsyncSequence<Base.Element>.AsyncIterator
+    {
         let id = UUID().uuidString
         let sequence = AsyncThrowingStream<Element, Error> {
             $0.onTermination = { @Sendable _ in
                 self.remove(id)
             }
-            
+
             await self.add(id: id, continuation: $0)
         }
-        
+
         return sequence.makeAsyncIterator()
     }
 
     // MARK: Sequence management
-    
+
     nonisolated private func remove(_ id: String) {
         Task {
             await self._remove(id)
         }
     }
-    
+
     private func _remove(_ id: String) {
         self.continuations.removeValue(forKey: id)
+        if selfTerminate && self.continuations.isEmpty {
+            self.subscriptionTask = nil
+        }
     }
-    
-    private func add(id: String, continuation: AsyncThrowingStream<Base.Element, Error>.Continuation) {
+
+    private func add(
+        id: String, continuation: AsyncThrowingStream<Base.Element, Error>.Continuation
+    ) {
         self.continuations[id] = continuation
         self.subscribeToBaseSequenceIfNeeded()
     }
-    
+
     private func subscribeToBaseSequenceIfNeeded() {
         guard self.subscriptionTask == nil else { return }
+        let selfTerminate = self.selfTerminate
 
         self.subscriptionTask = Task { [weak self, base] in
             guard let self = self else { return }
@@ -192,10 +204,19 @@ fileprivate actor SubSequenceManager<Base: AsyncSequence> where Base: Sendable {
             }
 
             do {
-                for try await value in base {
-                    await self.continuations.values.forEach { $0.yield(value) }
+                if selfTerminate {
+                    for try await value in base {
+                        guard await !self.continuations.isEmpty else {
+                            break
+                        }
+                        await self.continuations.values.forEach { $0.yield(value) }
+                    }
+                } else {
+                    for try await value in base {
+                        await self.continuations.values.forEach { $0.yield(value) }
+                    }
                 }
-                
+
                 await self.continuations.values.forEach { $0.finish() }
             } catch {
                 await self.continuations.values.forEach { $0.finish(throwing: error) }
@@ -208,7 +229,8 @@ fileprivate actor SubSequenceManager<Base: AsyncSequence> where Base: Sendable {
 
 extension AsyncSequence {
     /// Creates a shareable async sequence that can be used across multiple tasks.
-    public func shared() -> SharedAsyncSequence<Self> where Self: Sendable {
-        .init(self)
+    public func shared(selfTerminate: Bool = false) -> SharedAsyncSequence<Self>
+    where Self: Sendable {
+        .init(self, selfTerminate: selfTerminate)
     }
 }
